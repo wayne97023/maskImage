af2abc6b644a2a2835d85682a0c4dc0b4e7ec017
Lucas <wayne97023@gmail.com>
2020-03-14 17:12:14 +0800

    DogMask遮罩範例

--- maskImage/ViewController.swift
+++ maskImage/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var maskImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func setMask(_ sender: UIButton) {
+        maskImage.image = sender.currentImage
+        let dogImage = UIImage(named: "dog_mask")
+        let dogImageView = UIImageView(image: dogImage)
+        dogImageView.frame = CGRect(x: 0, y: 0, width: 414, height: 495)
+        maskImage.mask = dogImageView
+        print(maskImage)
+    }
+    
+    
 }
 
